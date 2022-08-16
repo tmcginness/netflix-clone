4bf1bb207de85402e376abc2eddf76cb5def28c9
Ted McGinness <ttmcginness22@gmail.com>
2022-08-16 14:26:56 -0500

    navbar styling begin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/app';
 
+const theme = createTheme({});
+
 ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
 
-  <BrowserRouter>
-
-    <App />
-  </BrowserRouter>,
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
